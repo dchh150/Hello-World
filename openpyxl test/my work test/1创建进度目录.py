b16b2e6c00c9d93964c185ff618f7036025c0da5
dchh150 <dchh150@126.com>
2018-06-26 10:45:58 +0800

    创建进度目录完成

--- openpyxl test/my work test/1创建进度目录.py
+++ openpyxl test/my work test/1创建进度目录.py
@@ -10,23 +10,29 @@ def my_mkdir(path_s):
     except FileExistsError as err:
         return print("目录已存在：", err)
     except FileNotFoundError as err:
-        return print("创建失败：",err,"\n上一级目录不存在\n-----")
+        return print("创建失败：", err, "\n上一级目录不存在\n-----")
 
 
 def main():
     today_ti = datetime.datetime.now()
-    today = today_ti.strftime("%Y%m%d")    #'20180625'
-    before14_ti = today_ti-datetime.timedelta(days=14)
-    before14 = before14_ti.strftime("%Y%m%d")    #'20180611'
+    today = today_ti.strftime("%Y%m%d")  # '20180625'
+    before7_ti = today_ti-datetime.timedelta(days=7)
+    before7 = before7_ti.strftime("%Y%m%d")  # '20180611'
     mypath = 'E:/2017工作-ET/开网优化/进度统计/每周进度/'
     mytodaypath = mypath+today
-    mybf14path = mypath+before14
+    mybf7path = mypath+before7
     # 当天文件夹
-    mypath2_li = ['/ftp', '/厂家反馈核查', '/发给厂家', '/建设维护类挂起工单', '/进度邮件']
-    # 子文件夹
-    copynames = [  #需要复制的文件，格式：日期用{}代替,开头需要/符号
-        '/a{}',
-        '/a{}',
+    mypath2_li = ['/ftp', '/厂家反馈核查', '/发给厂家',
+                  '/建设维护类挂起工单', '/进度邮件', '/发给厂家/厂家周四反馈']
+    # 创建的子文件夹
+    copynames = [  # 需要复制的文件，格式：日期用{}代替,开头需要/符号；
+        '/过程管控周报统计-{}-有fdd.xlsx',
+        '/每日质量通报统计{}-新版本-无fdd.xlsx',
+        '/进度邮件/过程管控指标周报V9-{}.xlsx',
+        '/进度邮件/开网优化进展,验收工作进度情况,各期锁定进度情况_{}.docx',
+        '/进度邮件/每日质量通报{}.xlsx',
+        '/发给厂家/附件一：21地市单优形象进度原因解析-{}.xlsx',
+        '/厂家反馈核查/添加前缀_厂家.bat'
     ]
     my_mkdir(mytodaypath)
     for each in mypath2_li:
@@ -36,11 +42,14 @@ def main():
     for n in copynames:
         try:
             shutil.copyfile(
-                mytodaypath + n.format(before14),
-                mybf14path + n.format(today)
+                mybf7path + n.format(before7),
+                mytodaypath + n.format(today)
             )
+            print("完成复制：", n.format(before7))
         except FileNotFoundError as err:
             print('复制错误,找不到原文件：\n', err)
+
+    input('操作完成，按任意键退出:')
 
 
 if __name__ == '__main__':
