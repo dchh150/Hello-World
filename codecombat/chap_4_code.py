efead69107527d71afa7748da46875620813820b
dchh150 <dchh150@126.com>
2019-03-01 17:36:40 +0800

    codecombat 4

--- codecombat/chap_4_code.py
+++ codecombat/chap_4_code.py
@@ -12,3 +12,35 @@ def summonAttack():
         for soldier in soldiers:
             if soldier:
                 hero.command(soldier, "attack", enemy)    # 使用 attack 命令让你的士兵们攻击。
+
+
+def findBestCoin(coins = None , moveTo = False , people = hero):
+    """
+    数组中寻找最合适的金币
+    moveTo == False ,返回金币对象；True ,英雄移动去获取金币
+    """
+    bestCoin = None
+    maxRating = 0
+    if coins == None:
+        coins = people.findItems()
+    # 试着计算"价值/距离"来决定你要收集哪个金币。
+    for coin in coins:
+        coinRating = coin.value / people.distanceTo(coin)
+        if coinRating > maxRating:
+            maxRating = coinRating
+            bestCoin = coin
+    if bestCoin == None:
+        bestCoin = people.findNearestItem()
+    if moveTo and bestCoin:
+        people.moveXY(bestCoin.pos.x , bestCoin.pos.y)
+    else:
+        return bestCoin
+
+
+def summonDiff(summonTypes = ["soldier","soldier","archer"]):
+    #按summonTypes 序列召唤
+    summontype = summonTypes[len(hero.built) % len(summonTypes)]
+    if hero.gold > hero.costOf(summontype):
+        hero.summon(summontype)
+
+
